028dd7fbc132ed26754927a45fbf17cc0248eaa1
RitaZab <98622430+RitaZab@users.noreply.github.com>
2022-06-08 13:18:45 +0200

    Add files via upload
    Adding new version with audio

--- Text-Morse_Conventer.py
+++ Text-Morse_Conventer.py
@@ -1,3 +1,6 @@
+import time
+import pygame
+
 morse_code = {' ': '_', "'": '.----.', '(': '-.--.-', ')': '-.--.-', ',': '--..--', '-': '-....-', '.': '.-.-.-',
               '/': '-..-.', '0': '-----', '1': '.----', '2': '..---', '3': '...--', '4': '....-', '5': '.....',
               '6': '-....', '7': '--...','8': '---..', '9': '----.', ':': '---...', ';': '-.-.-.', '?': '..--..',
@@ -7,13 +10,31 @@ morse_code = {' ': '_', "'": '.----.', '(': '-.--.-', ')': '-.--.-', ',': '--..-
               'Y': '-.--', 'Z': '--..', '_': '..--.-'}
 
 again=True
+time_between=0.5
+source="morse_code_audio/"
 
 def con_to_morse(given_text):
-    '''Converting function from text to morse code'''
+    '''Converting function from text to text version of morse code'''
     morse_text=[]
     for symbol in given_text:
         morse_text.append(morse_code[symbol])
     return " ".join(morse_text)
+
+def audio_convert(given_text):
+    '''Converting from text given by user to audio version of morse code'''
+    pygame.init()
+    for symbol in given_text:
+        if symbol ==" ":
+            print(" " *2, end=" ")
+            time.sleep(6*time_between)
+        else:
+            print(morse_code[symbol.upper()],end=" ")
+            pygame.mixer.music.load(source+symbol+'_morse_code.ogg')
+            pygame.mixer.music.set_volume(0.2)
+            pygame.mixer.music.play()
+            time.sleep(2*time_between)
+
+
 
 def play_again():
     global again
@@ -30,8 +51,16 @@ def play_again():
 
 
 while again:
-    given_text = input("Type text you want to translate (use only symbols from latin alphabet):\n").upper()
-    morse=con_to_morse(given_text)
-    print(morse)
-    play_again()
-
+    choice=input("You wan to translate for text or audio? Type text/audio ...").lower()
+    if choice=="text":
+        given_text = input("Type text you want to translate (use only symbols from latin alphabet):\n").upper()
+        morse=con_to_morse(given_text)
+        print(morse)
+        play_again()
+    elif choice=="audio":
+        given_text = input("Type text you want to translate (use only symbols from latin alphabet):\n").upper()
+        morse=audio_convert(given_text)
+        play_again()
+    else:
+        print("You need to type text or audio ...")
+        choice = input("You wan to translate for text or audio? Type text/audio ...").lower()
